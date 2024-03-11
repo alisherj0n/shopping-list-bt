66e61e657f64aad7be7c9123a1e9bc64decaa123
alisherj0n <alibrokhimov@gmail.com>
2024-03-11 15:28:44 +0500

    Feat: Remove item from local storage

--- script.js
+++ script.js
@@ -80,14 +80,33 @@ const getItemsFromStorage = () => {
   return itemsFromStorage;
 };
 
-const removeItem = e => {
+const onClickItem = e => {
   if (e.target.parentElement.classList.contains('remove-item')) {
-    if (confirm('Are you sure ?')) {
-      e.target.parentElement.parentElement.remove();
-
-      checkUI();
-    }
+    removeItem(e.target.parentElement.parentElement);
   }
+};
+
+const removeItem = item => {
+  if (confirm('Are you sure')) {
+    // Remove item from DOM
+    item.remove();
+
+    // Remove item from Storage
+
+    removeItemFromStorage(item.textContent);
+
+    checkUI();
+  }
+};
+
+const removeItemFromStorage = item => {
+  let itemsFromStorage = getItemsFromStorage();
+
+  // Filter out item to be removed.
+  itemsFromStorage = itemsFromStorage.filter(i => i !== item);
+
+  // Re-set to localstorage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
 };
 
 const clearItems = () => {
@@ -96,6 +115,9 @@ const clearItems = () => {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+
+  // Clear from localstorage
+  localStorage.removeItem('items');
 
   checkUI();
 };
@@ -131,7 +153,7 @@ const checkUI = () => {
 const init = () => {
   // Event Listeners
   itemForm.addEventListener('submit', onAddItemSubmit);
-  itemList.addEventListener('click', removeItem);
+  itemList.addEventListener('click', onClickItem);
   clearBtn.addEventListener('click', clearItems);
   itemFilter.addEventListener('input', filterItems);
   document.addEventListener('DOMContentLoaded', displayItems);
